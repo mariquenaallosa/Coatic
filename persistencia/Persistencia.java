c587a76a883dfd3e8917a5a25520ae1af0f8ccd5
Mariquena <allosamariquena@gmail.com>
2023-07-23 13:46:41 -0300

    Persistencia

---
+++ persistencia/Persistencia.java
@@ -0,0 +1,51 @@
+package persistencia;
+
+
+import javax.persistence.*;
+
+
+public class Persistencia {
+    
+    private final EntityManagerFactory emf;
+    private final EntityManager em;
+    
+    public Persistencia(){
+        this.emf = Persistence.createEntityManagerFactory("CoaticPU");
+        this.em = emf.createEntityManager();
+    }
+    
+    
+    // funciones necesarias para transacciones referidas a la base de datos
+    
+    public void insertar(Object o){
+        this.em.getTransaction().begin();
+        this.em.persist(o);
+        this.em.getTransaction().commit();  
+    }
+    
+    
+    public void modificar(Object o){
+        this.em.getTransaction().begin();
+        this.em.merge(o);
+        this.em.getTransaction().commit();
+    }
+    
+    
+     public void eliminar(Object o){
+        this.em.getTransaction().begin();
+        this.em.remove(o);
+        this.em.getTransaction().commit();
+    }
+    
+     
+     public Object buscar(Class clase, Object id){
+         Object o = em.find(clase, id);
+         return o;
+     }
+     
+     
+     public void refrescar(Object o){
+         this.em.refresh(o);
+     }
+    
+}
